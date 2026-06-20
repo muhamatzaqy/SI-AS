34cfd108bd98412fc321d565b0069e225f49a2e9
muhamatzaqy <157302474+muhamatzaqy@users.noreply.github.com>
2026-06-20 15:30:25 +0700

    Update page.tsx
    update import page laporan pgll

--- src/app/(dashboard)/pengelola/laporan/page.tsx
+++ src/app/(dashboard)/pengelola/laporan/page.tsx
@@ -9,6 +9,7 @@ import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Badge } from '@/components/ui/badge'
+import { Skeleton } from '@/components/ui/skeleton' // <--- Tambahkan baris ini
 import { useToast } from '@/hooks/use-toast'
 import { FileSpreadsheet, FileText, Loader2 } from 'lucide-react'
 import { formatDate, formatCurrency, formatLabel, calcAttendancePercentage } from '@/lib/utils'
