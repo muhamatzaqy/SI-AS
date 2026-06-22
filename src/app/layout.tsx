1b9a9412155fb2a95dd90159dc964a25f5fed72d
muhamatzaqy <157302474+muhamatzaqy@users.noreply.github.com>
2026-06-22 21:59:14 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { cn } from "@/lib/utils";
 
-// 1. IMPORT WADAH TOAST YANG SUDAH ADA DI PROYEK ANDA
-import { Toaster } from "@/components/ui/toaster"
+// UBAH BARIS INI: Ambil Toaster dari folder shared tempat Anda menyimpannya dulu
+import { Toaster } from "@/components/shared/toast-provider" 
 
 const geistSans = GeistSans;
 const geistMono = GeistMono;
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         
-        {/* 2. LETAKKAN TOASTER DI SINI */}
+        {/* WADAH TOAST TETAP DI SINI */}
         <Toaster />
         
       </body>
