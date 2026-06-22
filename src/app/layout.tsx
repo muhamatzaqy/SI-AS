ab1b4c9a6fab8f4c9a35772eebf2b1ac7ae6ca9a
muhamatzaqy <157302474+muhamatzaqy@users.noreply.github.com>
2026-06-22 23:55:50 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, Viewport } from 'next'
-import './globals.css'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { cn } from "@/lib/utils";
